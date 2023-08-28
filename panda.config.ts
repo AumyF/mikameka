b0702cea5d7855a8497e8c1a54356f8233168a1f
Aumy <aumy@outlook.jp>
2023-08-29 02:03:27 +0900

    strict token

--- panda.config.ts
+++ panda.config.ts
@@ -4,6 +4,8 @@ export default defineConfig({
   // Whether to use css reset
   preflight: true,
 
+  strictTokens: true,
+
   // Where to look for your css declarations
   include: ["./app/**/*.{js,jsx,ts,tsx}"],
 
