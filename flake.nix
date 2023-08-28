49064f20a807f9447ddbf8e9a4ae0edeb37a107a
Aumy <aumy@outlook.jp>
2023-08-29 00:55:13 +0900

    Setup

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "MIDI sequencer";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [bun act];
+        };
+      }
+    );
+}
